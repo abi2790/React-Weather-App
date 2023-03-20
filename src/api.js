e6d77be08f39778a1dad2ca6e42a33e51b1ac899
Abishek K <94035638+abi2790@users.noreply.github.com>
2023-03-20 20:26:31 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+      "X-RapidAPI-Key": '97da9359a4msh9a19e8764ac89ffp1fe690jsn8d54e7e3143d',
+      "X-RapidAPI-Host": 'wft-geo-db.p.rapidapi.com',
+    },
+  };
+  export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "dae71e2b1e21525bc73cd7d2a2994e05";
